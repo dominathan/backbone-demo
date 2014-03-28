0f235ffb0859458537a887c310844dcb05cec807
Nathan Hall <nathan.mh@gmail.com>
2014-03-28 10:26:04 -0500

    initialized backbone demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackboneDemo::Application.config.secret_key_base = '3efa349890032cf3e653fd8703e9189dd6c1b7b349b9a283e818424311b95e5281cdf6bd079c6f7131a187337a61776de9ab84ca0a65bfb8fa89f28f9c43d9bc'
